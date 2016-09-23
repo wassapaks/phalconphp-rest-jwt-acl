99e27ad0234c6d8903b9c3c2edb3fb8e2c1aa589
Efren Bautista Jr <Apple@Apples-MacBook-Pro.local>
2016-09-23 16:14:48 +0800

    update unit test cases

---
+++ test/UserRoute.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apple
+ * Date: 23/09/2016
+ * Time: 4:02 PM
+ */
+
+namespace Test\Route;
+
+
+class UserRoute
+{
+
+}
---
+++ test/app/config/ConfigTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apple
+ * Date: 23/09/2016
+ * Time: 2:26 AM
+ */
+
+namespace Test\App\Config;
+
+use \PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    public function testEmpty()
+    {
+
+        $this->assertTrue(true);
+        return 'first';
+
+    }
+}
